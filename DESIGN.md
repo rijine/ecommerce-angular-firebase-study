427ebf124ce7d3027239e62293923780518d40d4
rijine <e.rijin@gmail.com>
2018-03-27 00:10:16 +0530

    searching and indexing + readme

---
+++ DESIGN.md
@@ -0,0 +1,14 @@
+# User
+## User Registration
+  > Email & Password Sign Up
+  - createUserWithEmailAndPassword()
+    - Initiate registration
+    - Store to inCompleteResiter/unknownUsers collection
+    - Funciton to Clean entry after every 3 days/1 week
+    - Function to send Email request
+    - Angular: Expose page to accept activation url with token
+    -
+  2.
+
+
+# Query
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +17,83 @@ export class AppComponent {
   title = 'app';
   email = '';
   pwd = '';
+  filterField = '';
+
+  product = {
+    _url: 'Procut2',
+    name: 'name12',
+    title: 'title',
+    desc: 'desc',
+    unit: 'unit',
+    minQty: '10',
+    images: [
+      {
+        imagePath: 'url to path',
+        desc: 'desc',
+        thumbPath: 'url'
+      }
+    ],
+    exporterId: '598419db84c0df1ec531b6e1',
+    category: 'Agriculture',
+    subCategory: 'Sub Category',
+    item: 'Item'
+  };
 
   items: Observable<any[]>;
+  userInfo: Observable<any[]>;
+  prodcutList: Observable<any[]>;
+  private products: AngularFirestoreCollection<any>;
 
   // auth
 
-  constructor(db: AngularFirestore, private afAuth: AngularFireAuth) {
+  constructor(private db: AngularFirestore, private afAuth: AngularFireAuth) {
     this.items = db.collection('categories').valueChanges();
+    this.userInfo = db.collection('users').valueChanges();
+    this.prodcutList = db.collection('products').valueChanges();
+    this.products = db.collection('products');
+  }
+
+  addProduct() {
+    this.products.add(this.product);
+  }
+
+  search1() {
+    const query$ = new Subject();
+    const queryObservable = query$.switchMap(() =>
+      this.db
+        .collection('products', ref =>
+          ref.where('category', '==', 'Agriculture')
+          .orderBy('name')
+          .startAt(this.filterField)
+        )
+        .valueChanges()
+    );
+
+    queryObservable.subscribe( (items) => {
+      console.log(items);
+    });
+
+    query$.next();
+  }
+
+  search2() {
+    const query$ = new Subject();
+    const queryObservable = query$.switchMap(() =>
+      this.db
+        .collection('products', ref =>
+          ref.where('category', '==', 'Agriculture')
+          .where('subCategory', '==', 'Fruits')
+          .orderBy('name')
+          .startAt('abcd')
+        )
+        .valueChanges()
+    );
+
+    queryObservable.subscribe( (items) => {
+      console.log(items);
+    });
+
+    query$.next();
   }
 
   register() {
